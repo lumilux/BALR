f035068b7371a74d83fabd85eab60e28bd819013
octopi <octopie@gmail.com>
2012-04-24 01:55:47 -0400

    preliminary extension working: detects dead URLs, redirects to our own BALR page

---
+++ balr/extension/background.js
@@ -0,0 +1,20 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(changeInfo.status === 'loading') {
+		var url = tab.url;
+
+		// only worry about http links
+		// and if the balr flag is false, don't do anything
+		if(url.match(/^https/) === null && url.match(/^http/).length > 0
+			&& url.match(/\?.*nobalr/) === null) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('HEAD', url, true);
+			xhr.onloadend = function() {
+				if(xhr.status === 0) {
+					// if there's a bad status, we redirect to BALR's page
+					chrome.tabs.update({'url': chrome.extension.getURL('balr.html?'+url)});
+				}
+			};
+			xhr.send(null);
+		}
+	}
+});
---
+++ balr/extension/balr.js
@@ -0,0 +1,10 @@
+// var links = document.querySelectorAll('a');
+// console.log('links: ', links);
+// for(var i = 0; i < links.length; i++) {
+
+// }
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// 	console.log(tabId+" has changeInfo of "+changeInfo);
+// });
+
