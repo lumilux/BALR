499f601981c29ae214c9d1fdbe1ec6a644e4bfd9
octopi <octopie@gmail.com>
2012-04-24 23:04:23 -0400

    added check to make sure the dead link really is down and not just a network d/c (ping personal server)

--- balr/extension/background.js
+++ balr/extension/background.js
@@ -11,7 +11,19 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			xhr.onloadend = function() {
 				if(xhr.status === 0) {
 					// if there's a bad XHR status, we redirect to BALR's page
-					chrome.tabs.update({'url': chrome.extension.getURL('balr.html?'+url)});
+
+					// first we check to see if the user's network connectivity is up though, 
+					// with a call to google
+					var networkXhr = new XMLHttpRequest();
+					networkXhr.open('HEAD', 'http://davidhu.me', true);
+					networkXhr.onloadend = function() {
+						if(networkXhr.status !== 0) {
+							// if no error, then the original website really is down and we show BALR suggestions
+							// if networkXhr gets a non-error status code, just render chrome's default
+							chrome.tabs.update({'url': chrome.extension.getURL('balr.html?'+url)});
+						}
+					}
+					networkXhr.send(null);
 				}
 			};
 			xhr.send(null);
