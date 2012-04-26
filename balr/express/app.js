617bb072fbcc5c6f4ed1c46fc53e245c40f65705
Hans E Hyttinen <hans@lumilux.org>
2012-04-25 23:24:43 -0400

    implemented GET. added redis.

--- balr/express/app.js
+++ balr/express/app.js
@@ -4,10 +4,13 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , querystring = require('querystring');
 
-var redis = require('redis'),
+var redis = require('redis')
   , r = redis.createClient();
+
+r.select(4);
 
 var app = module.exports = express.createServer();
 
@@ -39,19 +42,30 @@ app.listen(3000, function(){
 });
 
 app.get('/alts/:dead_url', function(req, res, next) {
-  dead_url = req.params.dead_url;
+  dead_url = querystring.unescape(req.params.dead_url);
+  //console.log(req.params.dead_url);
+  console.log(dead_url);
   // return status, dead_location, alternatives
-  if r.sismember('dead_locs', dead_url) 
-    && r.hexists('alts:'+dead_url) {
-    r.hgetall('alts:'+dead_url, function(err, obj) {
-      if(err) return next(new Error('could not get alternatives for: ' + dead_url));
-      alts_json = [];
-      for(var alt in obj) {
-        alts_json.push({url: alt, clicks: obj[alt]});
-      }
-      res.json(alts_json);
-    });
-  } else {
-    return next(new Error('dead url not in db: ' + dead_url));
-  }
+   
+  r.sismember('dead_locs', dead_url, function(err, reply) {
+    if(reply) {
+      r.exists('alts:'+dead_url, function(err, reply) {
+        if(reply) {
+          r.hgetall('alts:'+dead_url, function(err, obj) {
+            if(err) return next(new Error('could not get alternatives for: ' + dead_url));
+            alts_json = [];
+            for(var alt in obj) {
+              alts_json.push({url: alt, clicks: obj[alt]});
+            }
+            res.json(alts_json);
+          });
+        } else {
+          return next(new Error('dead url not in alts: ' + dead_url));
+        }
+      });
+    } else {
+      return next(new Error('dead url not in dead_locs: ' + dead_url));
+    }
+  });
+  console.log('wat.');
 });
