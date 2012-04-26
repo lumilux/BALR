172e7c58eae6f3bc5cbaedbeb6e87c21763c3b4d
Hans E Hyttinen <hans@lumilux.org>
2012-04-25 22:26:03 -0400

    implemented get

--- balr/express/app.js
+++ balr/express/app.js
@@ -6,6 +6,9 @@
 var express = require('express')
   , routes = require('./routes');
 
+var redis = require('redis'),
+  , r = redis.createClient();
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -33,4 +36,22 @@ app.get('/', routes.index);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
+
+app.get('/alts/:dead_url', function(req, res, next) {
+  dead_url = req.params.dead_url;
+  // return status, dead_location, alternatives
+  if r.sismember('dead_locs', dead_url) 
+    && r.hexists('alts:'+dead_url) {
+    r.hgetall('alts:'+dead_url, function(err, obj) {
+      if(err) return next(new Error('could not get alternatives for: ' + dead_url));
+      alts_json = [];
+      for(var alt in obj) {
+        alts_json.push({url: alt, clicks: obj[alt]});
+      }
+      res.json(alts_json);
+    });
+  } else {
+    return next(new Error('dead url not in db: ' + dead_url));
+  }
 });
